40312c8016a032de6b1f42cdd4c6a841dca4b2d1
Stefan Foulis <stefan.foulis@sherpany.com>
2018-04-06 01:09:43 +0200

    update aliases when needed

--- network-joiner/dpnj.py
+++ network-joiner/dpnj.py
@@ -2,14 +2,13 @@ from collections import defaultdict
 
 import click
 import docker
-from pprint import pformat
 
 
 client = docker.from_env()
 
 
 def should_join_networks(proxy_name):
-    vhosts = defaultdict(list)
+    vhosts = defaultdict(set)
     for container in client.containers.list():
         if container.name == proxy_name:
             # Ignore the proxy container, since it does not make sense that
@@ -26,21 +25,21 @@ def should_join_networks(proxy_name):
             ]
             if not virtual_hosts:
                 continue
-            networks = list(container.attrs['NetworkSettings']['Networks'].keys())
+            networks = container.attrs['NetworkSettings']['Networks'].keys()
             for network in networks:
                 for virtual_host in virtual_hosts:
-                    vhosts[network].append(virtual_host)
+                    vhosts[network].add(virtual_host)
     return vhosts
 
 
-def already_joined_networks(proxy_name, proxy_network_name):
+def currently_joined_networks(proxy_name, proxy_network_name):
     proxy = client.containers.get(proxy_name)
     networks = {}
     for network_name, network in proxy.attrs['NetworkSettings']['Networks'].items():
         if network_name == proxy_network_name:
             # Ignore the proxies own network.
             continue
-        networks[network_name] = network['Aliases']
+        networks[network_name] = set(network['Aliases'])
     return networks
 
 
@@ -48,21 +47,27 @@ def sync_networks(proxy_name, proxy_network_name):
     should_join_dict = should_join_networks(
         proxy_name=proxy_name,
     )
-    already_joined_dict = already_joined_networks(
+    currently_joined_dict = currently_joined_networks(
         proxy_name=proxy_name,
         proxy_network_name=proxy_network_name,
     )
-    should = set(should_join_dict.keys())
-    joined = set(already_joined_dict.keys())
+    should_be_member_of = set(should_join_dict.keys())
+    is_member_of = set(currently_joined_dict.keys())
 
-    to_leave = joined - should
-    to_join = should - joined
-    already_joined = joined & should
+    to_leave = is_member_of - should_be_member_of
+    to_join = should_be_member_of - is_member_of
+    already_joined = is_member_of & should_be_member_of
     click.echo(
         f'to leave: {to_leave}\n'
         f'to join: {to_join}\n'
         f'already joined: {already_joined}\n'
     )
+    all_aliases = set()
+    for aliases in should_join_dict.values():
+        for alias in aliases:
+            all_aliases.add(alias)
+    click.echo(f'all aliases: {all_aliases}')
+
     for network_name in to_leave:
         network = client.networks.get(network_name)
         click.echo(f'disconnecting from {network_name}... ', nl=False)
@@ -72,8 +77,22 @@ def sync_networks(proxy_name, proxy_network_name):
     for network_name in to_join:
         network = client.networks.get(network_name)
         click.echo(f'connecting to {network_name}... ', nl=False)
-        network.connect(proxy_name)
+        network.connect(proxy_name, aliases=list(all_aliases))
         click.echo(f'done', color='green')
+
+    for network_name in already_joined:
+        current_aliases = currently_joined_dict[network_name]
+        if all_aliases.issubset(current_aliases):
+            continue
+        network = client.networks.get(network_name)
+        click.echo(f're-connecting to {network_name} to update aliases... disconnecting... ', nl=False)
+        network.disconnect(proxy_name, force=True)
+        click.echo(f'connecting... ', nl=False)
+        network.connect(proxy_name, aliases=list(all_aliases))
+        click.echo(f'done', color='green')
+        # We don't try to remove no longer needed aliases here because that
+        # would be much more complicated and leaving outdated aliases does no
+        # harm (I hope).
 
     if to_join:
         # nginx-proxy is a bit eager about marking a container as unreachable if
@@ -83,9 +102,14 @@ def sync_networks(proxy_name, proxy_network_name):
         # Unfortunatly nginx-proxy won't detect that the container would now be
         # reachable until the next time the nginx config is re-generated.
         # So here we want to tell nginx-proxy to re-generate the config files.
-        # We're taking the easiest way to trigger it: create a new container.
+        # We're taking the easiest and hackiest way to trigger it:
+        #   create a new container.
         click.echo(f'telling {proxy_name} to regenerate config... ', nl=False)
-        client.containers.run('busybox:latest', auto_remove=True)
+        client.containers.run(
+            'busybox:latest',
+            auto_remove=True,
+            name='proxy-regenerate-trigger',
+        )
         click.echo(f'done', color='green')
 
 
